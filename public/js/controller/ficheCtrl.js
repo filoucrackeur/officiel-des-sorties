6f50754ef5ee0a81839f3558674e007dc7ddbfd3
Philippe COURT <philippe.court@gmail.com>
2014-04-09 23:02:05 +0200

    MAH

---
+++ public/js/controller/ficheCtrl.js
@@ -0,0 +1,23 @@
+function ficheCtrl($scope){
+	$scope.participants = [{
+		nom: 'COURT',
+		prenom: 'philippe',
+		age: '28',
+		sexe: 'M',
+	},{
+		nom: 'COURT',
+		prenom: 'Laurence',
+		age: '25',
+		sexe: 'S',
+	},{
+		nom: 'COURT',
+		prenom: 'Marina',
+		age: '30',
+		sexe: 'S',
+	},{
+		nom: 'RICHARDS',
+		prenom: 'Denise',
+		age: '30',
+		sexe: 'S',
+	}]
+}
